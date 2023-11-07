7cdb57c6f19be705b4c33ccd4cf72d7dd47c18f7
Fareha SI <fareha.s@live.com>
2023-11-07 08:34:36 +0400

    initial

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    profile_picture = CloudinaryField('image', folder='profile_pics/', blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
     full_name = models.CharField(max_length=255, blank=True)
 
